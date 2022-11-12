28f1e5a4842bda51149c2652430d06827210e599
mohit.acharya19 <mohit.acharya19@gmail.com>
2022-11-12 17:55:16 +0530

    URL KEY UPDATE

--- sensor/constant/env_variable.py
+++ sensor/constant/env_variable.py
@@ -1,4 +1,4 @@
-MONGODB_URL_KEY = "MONGO_DB_URL"
+MONGODB_URL_KEY = "mongodb+srv://avnish:Aa327030@cluster0.or68e.mongodb.net/admin?authSource=admin&replicaSet=atlas-desfdx-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
 AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
 REGION_NAME = "us-east-1"
